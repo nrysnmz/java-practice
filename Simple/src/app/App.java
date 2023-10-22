ebd5049d3f5388f9bfc136c69ad3e75fb09b3726
Nuray Korganci <nrysnmz@gmail.com>
2023-10-22 12:02:54 -0500

    Changed Hello World 2

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 public class App{
     public static void main(String[]aStrings){
-        System.out.println("Hello World 1");
+        System.out.println("Hello World 2");
     }
 }
