5b53430b7a0ed3ea3d690e221a31f9529b5b74a5
Nuray Korganci <nrysnmz@gmail.com>
2023-10-22 12:01:25 -0500

    Changed to Hello World 1

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -2,6 +2,6 @@ package app;
 
 public class App{
     public static void main(String[]aStrings){
-        System.out.println("Hello World!!!!!");
+        System.out.println("Hello World 1");
     }
 }
